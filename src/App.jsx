7d1036331d95e9fa4f9d20d834b1a2b76ff85916
Nils Hartmann <nils@nilshartmann.net>
2016-11-16 19:46:15 +0100

    More examples

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,6 @@ import ContactsPage from './contacts/ContactsPage';
 
 import contactsModel from './contacts/ContactsModel';
 
-//
-
-
 const NoMatch = (props) => (
 	// There's a bug in <Miss />, so no props are passed in yet: https://github.com/ReactTraining/react-router/issues/4026
 	<div>Nothing matched!
@@ -20,6 +17,16 @@ const NoMatch = (props) => (
 		</pre>
 	</div>
 );
+
+const renderContactsPage = (props) => {
+	console.log('props', props);
+
+	// there's a bug with undefined optional props: https://github.com/ReactTraining/react-router/issues/4138
+	if (!props.params.pk || props.params.pk === 'undefined') {
+		return <Redirect to={`${props.pathname}/1`} />;
+	}
+	return <ContactsPage contacts={contactsModel.contacts} selectedContactPk={props.params.pk} />;
+}
 
 
 export default (props) => {
@@ -34,18 +41,9 @@ export default (props) => {
 			</div>
 		</header>
 
-		<Match pattern='/about' pattern="/about" component={AboutPage} />
-		<Match pattern='/contacts/:pk?' render={(props) => {
-			console.log('props', props);
-
-			// there's a bug with undefined optional props: https://github.com/ReactTraining/react-router/issues/4138
-			if (!props.params.pk || props.params.pk === 'undefined') {
-				return <Redirect to={`${props.pathname}/1`} />;
-			}
-			return <ContactsPage contacts={contactsModel.contacts} selectedContactPk={props.params.pk} />;
-		} }
-			/>
-
+		<Match exactly pattern='/' render={() => <Redirect to='/about' />} />
+		<Match pattern='/about' component={AboutPage} />
+		<Match pattern='/contacts/:pk?' render={renderContactsPage} />
 		<Miss component={NoMatch} />
 	</main>;
 };
--- src/about/AboutPage.jsx
+++ src/about/AboutPage.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-export default (props) => (
-	<div className='Page'>
-		<div className='title'><h1>About</h1></div>
-		<div className='Left'>React Router v4 Demo</div>
-		<div className='Right'>{JSON.stringify(props, null, 2)}</div>
-	</div>
+import Link from 'react-router/Link';
+import Match from 'react-router/Match';
+
+const Why = () => <div className='Full'><h1>Why?</h1>Just to show: <a href='https://github.com/ReactTraining/react-router/tree/v4'>React Router v4</a> already works!</div>;
+const Source = () => <div className='Full'><h1>Source</h1>You can find the Source Code at <a href='https://github.com/nilshartmann/react-rr4-example'>GitHub!</a></div>;
+const More = () => <div className='Full'><h1>More Informations</h1>You can find a lot more Informations and meet nice people at the <a href='http://www.meetup.com/de-DE/Hamburg-React-js-Meetup/'>React Meetup!</a></div>;
+
+export default () => (
+	<span>
+		<div className='Page'>
+			<div className='title'><h1>About</h1></div>
+			<div className='Full'>
+				This is a simple react Router v4 Demo. <b>Click to below for more informations.</b>
+
+ 				<div className='button-bar' style={{textAlign: 'left', marginTop: '1rem'}}>
+					<div className='button' style={{marginLeft: 0}}><Link to='/about/why'>Why?</Link></div>
+					<div className='button'><Link to='/about/source'>Source</Link></div>
+					<div className='button'><Link to='/about/more'>More</Link></div>
+				</div>
+			</div>
+		</div>
+
+		<div className='Page'>
+			<Match pattern='why' component={Why} />
+			<Match pattern='source' component={Source} />
+			<Match pattern='more' component={More} />
+		</div>
+	</span>
 );
